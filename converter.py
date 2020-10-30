50c30a299a929b2d0c65990a085dba1cf5c5a082
Zan Vajngerl <zvajngerl@gmail.com>
2020-10-30 16:21:00 +0100

    - updated converter.py to allow input discrepancies

--- converter.py
+++ converter.py
@@ -6,10 +6,12 @@ while True:
 
     miles = amount*0.621371
     kilometres = amount*1.60934
+    miles_list = ["m", "miles", "mile"]
+    kilometres_list = ["km", "kilometre", "kilometer", "kilometers", "kilometres", "k"]
 
-    if converted_unit == "m" or converted_unit == "miles":
+    if converted_unit in miles_list:
         print(f"{amount} kilometers is {miles:.2f} miles")
-    elif converted_unit == "km" or converted_unit == "kilometers" or converted_unit == "kilometres":
+    elif converted_unit in kilometres_list:
         print(f"{amount} miles is {kilometres:.2f} kilometres")
 
     reload = input("Would you like to do another conversion? y/n:" ).casefold()
